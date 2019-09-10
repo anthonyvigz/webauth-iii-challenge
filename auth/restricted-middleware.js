8456a287a2f1aecc75c9baf6628640aafa647e05
anthonyvigz <anthonyvigz@gmail.com>
2019-09-10 19:42:52 -0400

    set up the auth router

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -4,9 +4,10 @@ const bcrypt = require('bcryptjs');
 const Users = require('../models/users-model.js');
 const jwt = require('jsonwebtoken');
 
+// for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); 
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
 
   Users.add(user)
